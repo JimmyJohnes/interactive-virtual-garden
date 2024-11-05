2ad5727a82e922de63c3916fa9e9830fce36f39d
Skull Broz <skullbroz11@gmail.com>
2024-11-05 20:45:11 +0200

    more image fixes

--- TUIO11_NET-master (3)/TUIO11_NET-master/TuioDemo.cs
+++ TUIO11_NET-master (3)/TUIO11_NET-master/TuioDemo.cs
@@ -282,7 +282,7 @@ public class TuioDemo : Form, TuioListener
 	}
 	//string connectionString = "mongodb+srv://omarhani423:GcX8zgZnPP9TCHBD@cluster0.eqr9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 	private MongoDBHandler mongoDbOps = new MongoDBHandler("mongodb+srv://abdelrahmannader:callofdirt1@cluster0.ytujf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "Vitrula-garden");
-	public int scene = 0;
+	public int scene = 1;
 
 	public Bitmap small_shovel;
 	public Bitmap objectImage;
@@ -926,7 +926,7 @@ public class TuioDemo : Form, TuioListener
                         }
 						if (scene == 1)
 						{
-                            Rectangle ItemRECT = new Rectangle(ox - size/2, oy - size / 2, size, size);
+                            Rectangle ItemRECT = new Rectangle(ox - size / 2, oy - size / 2, size, size);
 
                             if (Store_Intersect(ItemRECT, RETURN.Rect))
                             {
